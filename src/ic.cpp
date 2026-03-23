95626207bed5850317a66430a15af41198adc548
Julie Gagnon <julie@umop.net>
2026-03-23 05:35:37 -0700

    explanation

--- src/ic.cpp
+++ src/ic.cpp
@@ -169,6 +169,7 @@ int main(int argc, char** argv)
         Progressbar pbr;
         pbr.maximum = 0;
         pbr.set_color(192, 64, 156);
+        cout << "Optimizing helix groups..." << endl;
         float anomaly = hg.optimize_helices(&p, 20, &pbr);
         cout << "Post-optimization anomaly: " << anomaly << endl;
     }
