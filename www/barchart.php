ae34658503a425d3e9a5957a751105c42e315ce7
Julie G <mournersmistake@primaryodors.org>
2026-04-16 12:02:07 -0700

    smh

--- www/barchart.php
+++ www/barchart.php
@@ -11,6 +11,13 @@ function lum($r, $g, $b)
     return $rcontrib*$r + $gcontrib*$g + $bcontrib*$b;
 }
 
+function sgn($n)
+{
+	if ($n > 0) return 1
+	if ($n < 0) return -1;
+	return 0;
+}
+
 $dispcolors = json_decode(file_get_contents("../data/dispcolor.json"), true);
 
 $bkcolor = [0x15, 0x1a, 0x37];
