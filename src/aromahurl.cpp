b61d70b7e26219627f5a92a424485e618f508539
Julie Gagnon <julie@primaryodors.org>
2026-04-29 06:57:11 -0700

    all the good names were taken fku

---
+++ src/aromahurl.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <regex>
+#include "classes/protein.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    Molecule m("jlgsux");
+    m.from_smiles("c1ccccc1CCOC(=O)C");
+
+    //
+}
